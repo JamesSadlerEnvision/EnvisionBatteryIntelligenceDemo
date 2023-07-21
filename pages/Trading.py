2723132755228a492c4ddb8a3ba306074fb4369d
James Sadler <james.sadler@envision-energy.com>
2023-07-21 09:11:56 +0100

    Trading first commit

---
+++ pages/Trading.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+st.markdown("<h1 style='text-align: center;'>Trading Optimisations</h1>", unsafe_allow_html=True)
+
